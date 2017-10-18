62f900f5d3129ae9e75ccc940648742e106811f8
andela-gike <grace.ike@andela.com>
2017-10-18 18:09:30 +0100

    Make async request to server side and render server validaton errors

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Form, Button } from 'semantic-ui-react';
+import { Form, Button, Message } from 'semantic-ui-react';
 import Validator from 'validator';
 import _ from 'lodash';
 import InlineError from '../messages/InlineError';
@@ -23,7 +23,9 @@ class LoginForm extends React.Component {
     const errors = this.validate(this.state.data);
     this.setState({ errors });
     if (_.isEmpty(errors)) {
-      this.props.submit(this.state.data);
+      this.setState({ loading: true});
+      this.props.submit(this.state.data)
+      .catch(err => this.setState({ errors: err.response.data.errors, loading: false }));
     };
     // if (Object.keys(errors).length === 0) {
     //   this.props.submit(this.state.data);
@@ -41,6 +43,10 @@ class LoginForm extends React.Component {
     const { data, errors } = this.state;
     return (
       <Form onSubmit={this.onSubmit}>
+        { errors.global && <Message negative>
+          <Message.Header>Something went wrong</Message.Header>
+          <p>{errors.global}</p>
+          </Message>}
         <Form.Field error={!!errors.email}>
           <label htmlFor="email">Email</label>
           <input
